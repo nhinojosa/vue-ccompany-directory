ef6dfa58436d9350a02b26111eea7838f8589662
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-11 21:45:07 -0500

    Style for navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@
             <div clase="menu">
                 <a href="#" class="menu-item">Departments</a>
                 <a href="#" class="menu-item">Settings</a>
-                <a href="#" class="menu-item">Logout</a>            
+                <a href="#" class="menu-login">Logout</a>            
             </div>
         </div>
     </nav>
@@ -20,9 +20,23 @@
 
 <style scoped lang="postcss">
     nav {
-        @apply h-20 bg-slate-900 text-slate-200;
+        @apply h-20 flex bg-slate-900 text-slate-200;
         .wrapper {
-            @apply flex items-center justify-between;
+            @apply container mx-auto flex w-full items-center justify-between;
+            .brand {
+                &-title {
+                    @apply text-2xl font-bold text-yellow-500;
+                }
+            }
+            .menu {
+                @apply flex gap-2;
+                &-item{
+                    @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+                }
+                &-login{
+                    @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700 hover:text-slate-900
+                }
+            }
         }
     }
 </style>
