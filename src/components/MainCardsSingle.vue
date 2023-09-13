2c7c22a1ae7138c21a10014814c4b99625316dea
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-12 20:59:26 -0500

    added card details with the use of faker

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,8 +1,20 @@
 <script setup>
-    import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
+const firstName = faker.name.firstName()
+const lastName = faker.name.lastName()
+const fullName = `${firstName} ${lastName}`  
 </script>
 
 <template>
-    <img :src="faker.internet.avatar()" alt="" srcset="" />
-    <span class=""> {{ faker.name.fullName() }}</span>
+    <div>
+        <div class="card-image">
+            <img :src="faker.internet.avatar()" alt="" srcset="" />
+        </div>
+        <div class="card-details">
+            <p class="card-details-name">{{ fullName }} </p>
+            <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
+            <p class="card-details-email"> {{ faker.internet.exampleEmail(firstName, lastName) }}</p>
+            <p class="card-details-quote"> {{ faker.lorem.paragraph( ) }}</p>
+        </div>
+    </div>
 </template>
