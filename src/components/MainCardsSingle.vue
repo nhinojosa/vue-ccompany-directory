025e2a7c309014f02071facf0d0340ff33af5e80
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-12 20:41:37 -0500

    Added faker and cards

---
+++ src/components/MainCardsSingle.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import { faker } from '@faker-js/faker'
+</script>
+
+<template>
+    <img :src="faker.internet.avatar()" alt="" srcset="" />
+    <span class=""> {{ faker.name.fullName() }}</span>
+</template>
