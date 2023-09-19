3fc05eaf0dfa866e06ac09d203c6eecb2dba1a4b
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-18 21:09:44 -0500

    mounted

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
-  import NavBar from './components/NavBar.vue';
-  import MainPage from './components/MainPage.vue';
+  import NavBar from '@/components/NavBar.vue';
+  import MainPage from '@/components/MainPage.vue';
+
+import { ref, onMounted } from 'vue'
+const showMainPage = ref(true)
+
+onMounted(() => {
+  if (windows.lcoation.pathname === '/') {
+    showMainPage.value = true
+  } else {
+    showMainPage.value = false
+  }
+})
+
+
 
 </script>
 
 <template>
   <NavBar />
-  <MainPage />
-  <NavMenu />
-  <RouterView />
+  <MainPage v-if="showMainPage" />
+  <div v-else>
+    <h1>This is another "Page"</h1>
+  </div>
 </template>
